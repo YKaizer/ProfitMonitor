1d1faa0faeb8b77bf84002e906bc29e358390ce4
YKaizer <163474883+YKaizer@users.noreply.github.com>
2025-06-07 13:25:53 +0400

    Add admin broadcast command

--- fsm/states.py
+++ fsm/states.py
@@ -6,3 +6,8 @@ class CommentState(StatesGroup):
 class AddServerState(StatesGroup):
     waiting_for_ip = State()
     waiting_for_name = State()
+
+
+class BroadcastState(StatesGroup):
+    """State for admin broadcast confirmation"""
+    waiting_for_confirm = State()
